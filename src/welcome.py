46e31d466648995975cd62ff412a55b3182e0e02
RODRIGO NEUMANN BARROS FERREIRA <rneumann@br.ibm.com>
2018-12-26 20:49:19 -0200

    Add Python deployment files

---
+++ src/welcome.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, jsonify
+from flask_restplus import Api, Resource
+
+app = Flask(__name__)
+api = Api(app, title='My first Python API', version='1.0', doc='/apidocs/',
+          description='A number-crunching API')
+
+
+@api.route('/double/<int:number>')
+@api.doc(params={'number': 'Number to be doubled.'}, description='This method doubles the input.')
+class DoubleNumber(Resource):
+    def get(self, number):
+        return jsonify(result=2 * number)
+
+
+port = os.getenv('PORT', '5000')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(port))
