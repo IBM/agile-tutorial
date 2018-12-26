abc514302343e2bd7da8a8fd15cef1b093471984
RODRIGO NEUMANN BARROS FERREIRA <rneumann@br.ibm.com>
2018-12-26 20:52:53 -0200

    Fix linter error

--- src/welcome.py
+++ src/welcome.py
@@ -3,12 +3,16 @@ from flask import Flask, jsonify
 from flask_restplus import Api, Resource
 
 app = Flask(__name__)
-api = Api(app, title='My first Python API', version='1.0', doc='/apidocs/',
+api = Api(app,
+          version='1.0',
+          doc='/apidocs/',
+          title='My first Python API',
           description='A number-crunching API')
 
 
 @api.route('/double/<int:number>')
-@api.doc(params={'number': 'Number to be doubled.'}, description='This method doubles the input.')
+@api.doc(params={'number': 'Number to be doubled.'},
+         description='This method doubles the input.')
 class DoubleNumber(Resource):
     def get(self, number):
         return jsonify(result=2 * number)
