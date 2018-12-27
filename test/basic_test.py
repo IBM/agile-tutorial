fa5d83fd8497fa2f3cc6726d7b03d6398ba5abb3
RODRIGO NEUMANN BARROS FERREIRA <rneumann@br.ibm.com>
2018-12-27 16:00:17 -0200

    Rename test file and use `to_json()` function

---
+++ test/basic_test.py
@@ -0,0 +1,10 @@
+from src.main import app
+from src.modules import basic_functions as bf
+
+
+def test_to_json():
+    with app.app_context():
+        assert bf.to_json(0).data == b'0\n'
+        assert bf.to_json([]).data == b'[]\n'
+        assert bf.to_json([1]).data == b'[1]\n'
+        assert bf.to_json([1, 2]).data == b'[1,2]\n'
--- test/test_basic.py
+++
@@ -1,6 +0,0 @@
-def test_5_equals_5():
-    assert 5 == 5
-
-
-def test_3_equals_5():
-    assert not 3 == 5
