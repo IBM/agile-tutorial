96f01b19233bacd86ae72db9ca4d468fadb030b5
RODRIGO NEUMANN BARROS FERREIRA <rneumann@br.ibm.com>
2018-12-27 11:27:13 -0200

    Rewrite API header and namespaces

--- src/welcome.py
+++ src/welcome.py
@@ -4,16 +4,27 @@ from flask import Flask, jsonify
 from flask_restplus import Api, Resource
 from werkzeug.contrib.fixers import ProxyFix
 
+# Setting up Python API
+my_list = list()
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
-api = Api(app, version='1.0', doc='/apidocs/', title='Agile Tutorial', description='Python API')
-my_list = list()
+api = Api(app,
+          version='1.0',
+          license='Apache-2.0',
+          title='Agile Tutorial',
+          contact='rneumann@br.ibm.com',
+          license_url='https://www.apache.org/licenses/LICENSE-2.0',
+          description='A Python API for manipulating a list of integer numbers',
+          )
+api.namespaces.clear()
 
-ns = api.namespace('basic', description='List manipulation')
+# Creating API namespaces
+basic_ns = api.namespace('basic', description='List manipulation')
+math_ns = api.namespace('math', description='Math operations')
 
 
-@api.route('/basic/print')
-@api.doc(description='Print current list.')
+@basic_ns.route('/print')
+@basic_ns.doc(description='Print current list.')
 class PrintList(Resource):
     def get(self):
         return jsonify(list=my_list)
