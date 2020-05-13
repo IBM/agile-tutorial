81fc64175500ec7b4b83bbe0ae1ba810d33d0dfb
RODRIGO NEUMANN BARROS FERREIRA <rneumann@br.ibm.com>
2020-05-13 18:53:11 -0300

    Remove added functions
    [skip ci]

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ from flask import Flask
 from flask_restplus import Api, Resource
 from werkzeug.contrib.fixers import ProxyFix
 
-from src import math_services as ms
-from src import basic_services as bs
 from src import default_services as ds
-from src import math_services as ms
 
 # Setting up Python API
 my_list = list()
@@ -19,18 +16,6 @@ api = Api(app, version='1.0', title='Agile Tutorial', description='A simple Pyth
 basic_ns = api.namespace('basic', description='List manipulation')
 math_ns = api.namespace('math', description='Math operations')
 
-@basic_ns.route('/print')
-@basic_ns.doc(description='Print list.')
-class PrintList(Resource):
-    def get(self):
-        return my_list
-
-@math_ns.route('/double')
-@math_ns.doc(description='Multiply list by 2.')
-class DoubleList(Resource):
-    def put(self):
-        return ms.double_list(my_list)
-
 
 @api.route('/answer')
 @api.doc(description='The Answer to the Ultimate Question of Life, the Universe, and Everything.')
@@ -38,114 +23,7 @@ class TheAnswer(Resource):
     def get(self):
         return ds.TheAnswerToLifeTheUniverseAndEverything()
 
-@basic_ns.route('/merge/<string:csv>')
-@basic_ns.doc(params={'csv': 'Comma-separated integer values.'}, description='Merge lists.')
-class MergeList(Resource):
-    def put(self, csv):
-        my_list.extend(bs.csv_to_list(csv))
-        return my_list
-        
-@basic_ns.route('/input/<string:csv>')
-@basic_ns.doc(params={'csv': 'Comma-separated integer values.'}, description='Inputs list.')
-class InputList(Resource):
-    def post(self, csv):
-        my_list.clear()
-        my_list.extend(bs.csv_to_list(csv))
-        return my_list
-        
-
-@basic_ns.route('/reverse')
-@basic_ns.doc(description='Reverse list.')
-class ReverseList(Resource):
-    def put(self):
-        my_list.reverse()
-        
-@basic_ns.route('/reset')
-@basic_ns.doc(description='Reset list.')
-class ResetList(Resource):
-    def delete(self):
-        my_list.clear()
-        return my_list
-        
-@basic_ns.route('/length')
-@basic_ns.doc(description='List length.')
-class LengthList(Resource):
-    def get(self):
-        return len(my_list)
-
-
-@basic_ns.route('/insert/<int:integer>/<int:position>')
-@basic_ns.doc(params={'integer': 'Integer value.', 'position': 'List position.'},
-              description='Inserts a single integer value at a given position in the list.')
-class InsertList(Resource):
-    def put(self, integer, position):
-        my_list.insert(position, integer)
-        return my_list
-
-@math_ns.route('/mean')
-@math_ns.doc(description='Mean value of the list.')
-class MeanList(Resource):
-    def get(self):
-        return ms.mean(my_list)
-
-
-@basic_ns.route('/sort')
-@basic_ns.doc(description='Sort list.')
-class SortList(Resource):
-    def put(self):
-        my_list.sort()
-        return my_list
-
-@basic_ns.route('/unique')
-@basic_ns.doc(description='Deduplicate list.')
-class UniqueList(Resource):
-    def put(self):
-        bs.unique(my_list)
-        return my_list
-
-
-@basic_ns.route('/count/<int:integer>')
-@basic_ns.doc(params={'integer': 'Integer value.'}, description='Count occurrences of a value.')
-class CountList(Resource):
-    def get(self, integer):
-        return my_list.count(integer)
-
-@basic_ns.route('/append/<int:integer>')
-@basic_ns.doc(params={'integer': 'Integer value.'}, description='Appends a single value.')
-class AppendList(Resource):
-    def put(self, integer):
-        my_list.append(integer)
-        return my_list
-
-
-@math_ns.route('/square')
-@math_ns.doc(description='Takes the square of the list.')
-class SquareList(Resource):
-    def put(self):
-        return ms.square_list(my_list)
-
-
-@math_ns.route('/min')
-@math_ns.doc(description='Min value of the list.')
-class MinList(Resource):
-    def get(self):
-        return min(my_list)
-
-
-@math_ns.route('/max')
-@math_ns.doc(description='Max value of the list.')
-class MaxList(Resource):
-    def get(self):
-        return max(my_list)
-
-@math_ns.route('/triple')
-@math_ns.doc(description='Multiply list by 3.')
-class TripleList(Resource):
-    def put(self):
-        return ms.triple_list(my_list)
-
 
 port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(port))
-
